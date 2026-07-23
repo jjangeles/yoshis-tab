d7e29ccb7586fe4a52b67db02681eba48e88442a
John Jerome Angeles <johnjeromeangeles@gmail.com>
2026-07-23 20:37:30 +0800

    Fix ios display error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#020617" />
+      </head>
       <body className="min-h-full flex min-h-screen flex-col bg-slate-50 text-slate-950 dark:bg-slate-950 dark:text-slate-50">
         <Providers>
           <main className="flex-1">{children}</main>
