e87fad3a51a6c7d395ad62f0903cdf848e39613b
John Jerome Angeles <johnjeromeangeles@gmail.com>
2026-07-29 10:47:41 +0800

    fix navigation position error

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,12 +6,13 @@ export default async function ProtectedLayout({ children }: { children: ReactNod
   const user = await getCurrentUser();
 
   return (
-    <div className="min-h-screen bg-slate-50 text-slate-950 dark:bg-slate-950 dark:text-slate-50">
-      <Header />
-
-      <main className="mx-auto max-w-3xl px-0 py-3 pb-24 sm:px-0">
+    <>
+      <main className="mx-auto min-h-screen max-w-3xl bg-slate-50 px-0 py-3 pb-28 text-slate-950 dark:bg-slate-950 dark:text-slate-50 sm:px-0">
         <div className="px-5">{children}</div>
       </main>
-</div>
+
+      {/* Render Header outside constrained containers */}
+      <Header />
+    </>
   );
 }
